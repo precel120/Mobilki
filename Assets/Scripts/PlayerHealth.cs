41aa4b6e66f88aedea80fb3e39d2e4c2aa188454
Maciej <darkuso@o2.pl>
2019-11-16 15:34:10 +0100

    moving left and right

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private int health;
+    public int Health { get { return health; } set { health = value; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    public float Speed;
+    private bool faceRight;
+    public Joystick joystick;
+    private Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        faceRight = true;
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = joystick.Horizontal;
+        Move(horizontal);
+        Flip(horizontal);
+    }
+
+    void Move(float horizontal)
+    {
+        rigidbody2D.velocity = new Vector2(horizontal * Speed, rigidbody2D.velocity.y);
+    }
+
+    void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !faceRight || horizontal < 0 && faceRight)
+        {
+            faceRight = !faceRight;
+
+            transform.Rotate(0f, 180f, 0f);
+        }
+    }
+}
